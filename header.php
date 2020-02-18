d09b4a064401ffe0af843cce65b70b6fdb9fbd1d
Victor Opere <victoropere@gmail.com>
2020-02-18 16:39:09 +0300

    just added the frontpage.php file

--- header.php
+++ header.php
@@ -15,11 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="profile" href="http://gmpg.org/xfn/11" />
-   
-
-
-
-
+  
     <?php if ( is_singular() && pings_open( get_queried_object(  ) )): ?>
 
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
